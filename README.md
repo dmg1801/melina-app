2f406a098cc62b7e1870ca9812943bc1d43fa1d7
Diego Mongay <mongaydiego@gmail.com>
2019-08-02 01:38:52 +0200

    more formular characteristics

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # melina-app
 formular application for kaninchenseele web page
+
+#git-hub-pages
+https://dmg1801.github.io/melina-app/client/main.html
--- client/src/main.js
+++ client/src/main.js
@@ -1,40 +1,47 @@
 (function () {
-    'use strict';
-    
-    angular.module('kaninchenApp', [])
+  'use strict';
+
+  angular.module('kaninchenApp', [])
     .controller('kaninchenController', kaninchenController);
-  
-    
-    kaninchenController.$inject = [];
-    function kaninchenController() {
-      var kaninchen = this;
 
-      kaninchen = {};
-      
-      //text variables
-      kaninchen.name = null;
-      kaninchen.weight = null;
-      kaninchen.city = null;
-      kaninchen.age = null;
-      
-      //text field
-      kaninchen.comment = [];
 
-      //boolean variables
-      kaninchen.rhd = false;
-      kaninchen.rhdTwo = false;
-      kaninchen.Myxomatose = false;
-      kaninchen.eCuniculiPositive = null;
-      kaninchen.eCuniculiNegative = null;
-      kaninchen.eCuniculiNotTested = null;
-      kaninchen.male = null;
-      kaninchen.female = null;
-      
-      //email
-      kaninchen.email = null;    
-     
-    }    
-   
-    
-    })();
-    
+  kaninchenController.$inject = [];
+
+  function kaninchenController() {
+    var kaninchen = this;
+
+    kaninchen = {};
+
+    //text variables
+    kaninchen.name = null;
+    kaninchen.weight = null;
+    kaninchen.city = null;
+    kaninchen.age = null;
+
+    //text field
+    kaninchen.comment = [];
+
+    //boolean variables
+    kaninchen.rhd = false;
+    kaninchen.rhdTwo = false;
+    kaninchen.Myxomatose = false;
+    kaninchen.eCuniculiPositive = null;
+    kaninchen.eCuniculiNegative = null;
+    kaninchen.eCuniculiNotTested = null;
+    kaninchen.male = null;
+    kaninchen.female = null;
+    kaninchen.inside = null;
+    kaninchen.outside = null;
+    kaninchen.both = null;
+    kaninchen.ensuranceYes = null;
+    kaninchen.ensuranceNo = null;
+
+    //email
+    kaninchen.email = null;
+    kaninchen.phoneNumber = null;
+
+
+  }
+
+
+})();
